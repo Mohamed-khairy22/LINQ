fc085d6362cd29981a1dd82eedf663cd6f00c483
mohamed-khairy <mohamedkhairy063036@gmail.com>
2024-09-19 06:33:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//using System.Linq;
+using System.Xml;
+using static L2O___D09.ListGenerators;
+namespace test_linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region WHERE is filrerition
+            // var result = ProductList.Where(p => p.UnitsInStock == 0);
+            // result = from p in ProductList
+            //             where p.UnitsInStock == 0
+            //             select p;
+            //result = ProductList.Where((p,i)=> (p.UnitsInStock == 0) && (i<=5));
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine("unit=" + item + " ,");
+            //}
+            #endregion
+            
+        }
+    }
+}
